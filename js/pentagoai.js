b25c463b393d13d19f454582afc4b34cee1bb0aa
Anthony Liu <anthonyscottliu@gmail.com>
2016-06-05 01:18:57 -0400

    Adds code to support rotation input.

--- js/pentagoai.js
+++ js/pentagoai.js
@@ -14,23 +14,89 @@ var PentagoAI = (function() {
 
     /****************
      * working vars */
-
-    /***********
-     * objects */
+    var state;
+    var turnState;
+    var currPlayer;
 
     /******************
      * work functions */
     function initPentagoAI() {
-      // generate grid
-      for (var xi = 0; xi < 6; xi++) {
+      state = [];
+
+      // generate ui grid
+      for (var yi = 0; yi < 6; yi++) {
         var div = document.createElement('div');
         div.className = 'col';
-        for (var yi = 0; yi < 6; yi++) {
+        state.push([]);
+        for (var xi = 0; xi < 6; xi++) {
           var sq = document.createElement('div');
           sq.className= 'color-square blank';
+          sq.id = 'sq'+xi+'-'+yi;
+          sq.addEventListener('click', function(e) {
+            placeMarble.apply(
+              this,
+              this.id.substring(2).split('-').map(function(a) {
+                return parseInt(a);   
+              }
+            ));
+          });
+
           div.appendChild(sq);
+          state[state.length-1].push(-1);
         }
         $s('#container').appendChild(div);
+      }
+
+      // init working vars
+      currPlayer = 0;
+      turnState = 0;
+
+      // add event listeners for the rotations
+      for (var cw = 0; cw < 2; cw++) {
+        for (var yi = 0; yi < 2; yi++) {
+          for (var xi = 0; xi < 2; xi++) {
+            $s('#r'+xi+'-'+yi+(cw === 0 ? 'ccw' : 'cw')).addEventListener(
+              'click',
+              (function(x, y, c) {
+                return function(e) {
+                  rotateBoard(x, y, c);
+                };
+              })(xi, yi, cw)
+            );
+          }
+        }
+      }
+    }
+
+    function rotateBoard(x, y, c) {
+      console.log(x, y, c);
+      if (turnState === 0) {
+        alert('First you must place a marble.');
+      } else {
+        // rotate now
+        console.log('rotate');
+        
+        // update turn state
+        currPlayer = 1 - currPlayer;
+        turnState = 0;
+        $s('#turn').innerHTML = currPlayer === 0 ? 'Red' : 'Blue';
+        $s('#what').innerHTML = 'place';
+      }
+    }
+
+    function placeMarble(x, y) {
+      if (state[y][x] < 0) {
+        if (turnState === 1 ) {
+          alert ('You already placed a marble this turn. Now you rotate.');
+        } else {
+          state[y][x] = currPlayer;
+          var cn = 'color-square ' + (state[y][x] === 0 ? 'red' : 'blue');
+          $s('#sq'+x+'-'+y).className = cn;
+          turnState += 1;
+          $s('#what').innerHTML = 'rotate';
+        }
+      } else {
+        alert('This cell has a marble in it!');
       }
     }
 
